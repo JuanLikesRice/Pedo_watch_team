147445a94beb45728d05837685482820305f4c4b
JuanLikesRice <72318652+JuanLikesRice@users.noreply.github.com>
2022-04-17 20:38:06 -0500

    working shi

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <msp430.h>
 #include <stdint.h>
 
-
+/*
 void delay_setup(){
       WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-      P1DIR |= 0x01;                            // P1.0 output
+      //                           // P1.0 output
       CCTL0 = CCIE;                             // CCR0 interrupt enabled
       CCR0 = 31250;                             // first time when interupts turns off
       TACTL = TASSEL_2 + MC_2 + ID_3;           // SMCLK, contmode
 }
-/*
+
 void delay(unsigned int time){
     CCR0 += time;
     __bis_SR_register(LPM0_bits + GIE);
@@ -24,12 +24,7 @@ void spi_setup(void)
     UCA0CTL0 |= UCSYNC; // spi
     UCA0CTL0 |= UCMST; //master
     UCA0CTL0 |= UCMSB; //MSB
-    //UCA0CTL0 |= UCMODE_1; //master
     UCA0BR0  |= 0x02; // /2 --> 500 khz OD THE SPI CLOCK
-   //UCA0CTL0 |= UCCKPH + UCMSB + UCMODE_1; // 4-pin, 8-bit SPI master
-   //UCA0CTL0 |= UCCKPH + UCMSB + UCMST + UCSYNC; // 3-pin, 8-bit SPI master
-    //UCA0BR1 = 0; //
-    //UCA0MCTL = 0; // No modulation
     UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
     P1SEL  |= BIT1;                               // Just setup for the pins (1.1 is the SOMI)
     P1SEL2 |= BIT1;
@@ -37,20 +32,38 @@ void spi_setup(void)
     P1SEL2 |= BIT2;
     P1SEL  |= BIT4;                               // Just setup for the pins (1.4 is the USCI clock)
     P1SEL2 |= BIT4;
+    P1DIR |= 0x01;
     P1DIR |= BIT0;// chip select
     P1OUT |= (BIT0);// VDD - on
 }
 
 
 
-void init_buttons() {
+void init_buttons(void) {
+    //  WDTCTL = WDTPW + WDTHOLD;
+    /*
+        P2DIR &= ~(BIT2); // set to input
+        //P2REN |= BIT2; // enable pullup/down resistors
+        //P2OUT |= BIT2; // set resistors to pull up
+        P2IES |= BIT2; // listen for high to low transitions
+        P2IFG &=  ~(BIT2); // clear any pending interrupts
+        P2IE |= BIT2; // enable interrupts for these pins
+*/
+
+        P2DIR |= 0x00;
+         P2OUT =  0x04;
+         P2REN |= 0x04;
+         P2IE |= 0x04;
+         P2IES |= 0x04;
+         //P2IFG &= ~0x04;
+
     /*P2DIR &= ~(BIT2); // set to input
     P2REN |= BIT2; // enable pullup/down resistors
     P2OUT |= BIT2; // set resistors to pull up
     P2IES |= BIT2; // listen for high to low transitions
     P2IFG &=  ~(BIT2); // clear any pending interrupts
     P2IE |= BIT2; // enable interrupts for these pins
-    */
+
     P2DIR = 0x00;
         P2OUT =  0x04;
         P2REN |= 0x04;
@@ -58,14 +71,24 @@ void init_buttons() {
         P2IES |= 0x04;
         P2IFG &= ~0x04;
 
+
+    P2SEL &= (~BIT2); // Set P2.1 SEL as GPIO
+    P2DIR &= (~BIT2); // Set P2.1 SEL as Input
+    P2IES |= (BIT2); // Falling Edge 1 -> 0
+    P2IFG &= (~BIT1); // Clear interrupt flag for P2.1
+    P2IE |= (BIT2); // Enable interrupt for P2.1
+   // __enable_interrupt(); // Enable Global Interrupts
+*/
+
+
+
 }
 
 
 
 void write1byte( unsigned int address_in, unsigned int value_in){
     P1OUT |= (BIT0);// VDD - on
-    //message[0] |= 0x80; // write flag
-    //UCA0TXBUF= 0x00; //us3ed to be dummy var
+
     while (!(IFG2 & UCA0TXIFG));
     P1OUT &= (~BIT0); //Off nCs
     UCA0TXBUF= address_in; ///address
@@ -95,8 +118,6 @@ unsigned int read1byte( unsigned int address_in){
     P1OUT |= (BIT0);// VDD - on
     address = address_in;
     address += 0x80; // write flag
-//    while (!(IFG2 & UCA0TXIFG));
-
     P1OUT &= (~BIT0); //Off nCs
     __delay_cycles(4);
     UCA0TXBUF= address; ///address
@@ -105,15 +126,7 @@ unsigned int read1byte( unsigned int address_in){
     UCA0TXBUF=  address; //value
     value2 = UCA0RXBUF;
     while (!(IFG2 & UCA0RXIFG));
-    //UCA0TXBUF=  address; //value
     value3 = UCA0RXBUF;
-    //while (!(IFG2 & UCA0RXIFG));
-    //value4 = UCA0RXBUF;
-
- //   UCA0TXBUF=  0x00; //value
-   // value = UCA0RXBUF;
-    //while (!(IFG2 & UCA0RXIFG));
-    //value2 = UCA0RXBUF;
     __delay_cycles(4);
     P1OUT |= (BIT0);// VDD - on nCS high
     __delay_cycles(100);
@@ -135,37 +148,26 @@ unsigned int steps;
 
 void main(void)
   {spi_setup();
-delay_setup();
+//delay_setup();
  __delay_cycles(10);
-// delay(10000);
 
 write1byte(0x1B,0x80); //CNTL2);
-//delay(1000);
 __delay_cycles(100);
 
 write1byte(0x1A,0x00);//CNTL1
 
-
 write1byte(0x44,0x2C);//CNTL2
 write1byte(0x37,0x7B);//CNTL
 
-
-
-write1byte(0x41,0x10); //THSHOLD low
+write1byte(0x41,0x06); //THSHOLD low
 write1byte(0x42,0x00); // ths high steps
-
 
 write1byte(0x24,0x20);//INC5 INT2 Enabled, active low
 write1byte(0x25,0x20);//INC6
 write1byte(0x26,0x20);//INC7 //step wm enabled INT 2
-
 // high =  read1byte(0x26);
 
-
 write1byte(0x20,0x00);//INC1 x20
-
-
-
 
 write1byte(0x43,0x66);//PED_CENTL1);
 write1byte(0x44,0x2C);//PED_CENTL2);
@@ -178,111 +180,66 @@ write1byte(0x4A,0x08);//PED_CENTL8);
 write1byte(0x4B,0x19);//PED_CENTL9);
 write1byte(0x4C,0x1C);//PED_CENTL10);
 
-
 write1byte(0x1A,0xC2);//CNTL1
-
-
 __delay_cycles(100);
-
 //delay(100);
-
-
 
 init_buttons();
 
 while(1)
 {
 
+    if ((P2IN & BIT2) == 0) {
+        __delay_cycles(100000);
+            low =  read1byte(0x0E);//KX126_PED_STEP_L);
+             high = read1byte(0x0F);
+             steps += low;
+    }
+
+    //__bis_SR_register(LPM3_bits + GIE);
+    __delay_cycles(10000);
 
 
-    __delay_cycles(1000000);
-    __delay_cycles(1000000);
-    __delay_cycles(1000000);
-    __delay_cycles(1000000);
-
-
-    //low =  read1byte(0x0E);//KX126_PED_STEP_L);
-
-      //  high = read1byte(0x0F);
-
-        //steps += low;
-
-
-
-
-    //delay(10);
-//    delay(1000);
-    //read5 = read1byte(0x1A);
-  //  delay(10);
-    //__delay_cycles(10000);
-
-    //low +=  read1step();//KX126_PED_STEP_L);
-// 0f -
-    //low =  read1byte(0x0E);//KX126_PED_STEP_L);
-    //high = read1byte(0x0F);
-
-    //steps += low;
-    //low =  read1byte(0x0E);//KX126_PED_STEP_L);
-    //low =  read1byte(0x0E);//KX126_PED_STEP_L);
-//    low =  read1byte(0x0E);//KX126_PED_STEP_L);
-
-    //__delay_cycles(1000000);
-
-    //high =  read1byte(0x0F);//KX126_PED_STEP_L);
-   // __delay_cycles(1000000);
-
-//    high =  read1byte(0x0E);
-  //  high =  read1byte(0x0E);
-    //high =  read1byte(0x0E);
-    // if (high > low) {
-   //steps +=low;
-
-     //}//
-//    low =  read1byte(0x0E);//KX126_PED_STEP_L);
-    //high = read1byte(0x0F);//KX126_PED_STEP_H);
-    //high = (read1byte(0x00F)<<8);//KX126_PED_STEP_H);
-//    if(low > 194){
-      //  low-=194;
-    //high_rs();
-    //steps+= low;
-
-
- //    high = read1byte(0x0F);
-    //}
-
-  //  read6 = high+low;
-    //if ( read6 != 0) {
-     //read7+= read6;
-     //}
-
-    //read6 = read_steps() ;
-//   var1 = read1byte(CNTL1_4);
-    //read3 = read1byte(0x37);
-
-  // var2 = read1byte(0x11);//who_asked);
-  // var1 = read1byte(0x1B);
-  // read1 = read1byte(0x1B);
-   //write1byte(0x43,0x10 +read1byte(0x43));
-   //   read7 = read_steps();
-
-   //var3 = read_steps();
-   //var6 = read1byte(CNTL1_2);
-    //read1byte(CNTL1_4);//read_steps();//read1byte(who_asked);
-  // if (allsteps != 0){
-    //   somesteps += allsteps;
-
-   //}
 }
 }
 
 
+/*
+//#pragma vector=PORT2_VECTOR
+//__interrupt void Port_2(void)
 
+// Port 2 interrupt service routine
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=PORT2_VECTOR
+__interrupt void button(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(PORT2_VECTOR))) button (void)
+#else
+#error Compiler not supported!
+#endif
+{   //buttons are used for changing between states
+   // P2IFG &= ~(BIT2) ;
+    __delay_cycles(100000);
+
+    //if ((P2IN & BIT2) == 0) {
+        low =  read1byte(0x0E);//KX126_PED_STEP_L);
+         high = read1byte(0x0F);
+         steps += low;
+
+         P2IFG &= ~0x04;
+//         __bis_SR_register(LPM4_bits);
+         //__bic_SR_register_on_exit(LPM4_bits);
+
+   // }
+
+    __bic_SR_register_on_exit(LPM3_bits);
+}
+
+
+/*
 
 __interrupt void Port_2(void)
 {
-    
-
-
     P2IFG &= ~0x04;
 
     if ((P2IN & 0x04) == 0) {//red
@@ -300,11 +257,11 @@ __interrupt void Port_2(void)
 
 
     __bic_SR_register_on_exit(LPM3_bits);
-    
-    
-    
-flag = 1;
-P1IFG &= (~BIT1); // P2.1 IFG clear
+
+
+
+//flag = 1;
+P2IFG &= (~BIT2); // P2.1 IFG clear
 }
 
 
